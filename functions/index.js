3023c4673b477b3d2ce4b7d984c118aeab01d3fc
abrauf123 <arauf@sysgain.com>
2019-08-28 16:51:30 +0530

    Final Version

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,21 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({origin: true});
 const mailgun = require("mailgun-js");
-
+const DOMAIN = 'YOUR_DOMAIN';
+const api_key = 'YOUR_API_KEY';
 const mg = mailgun({apiKey: api_key, domain: DOMAIN});
 
 exports.contactUs = functions.https.onRequest((req,res) => {
 
     return cors(req, res, () => {  
 
-        var reciepentAddress = req.body.recieveCopy ? req.body.email : 'abdulrauf76143@gmail.com';
-                const data = {
-                from: 'Angular Geeks <abdulrauf76143@gmail.com>',
-                to: reciepentAddress,
-                subject: 'Your copy of Mail is here !',
-                template: "email-demo",
-                "v:name": req.body.name,
-                "v:email": req.body.email,
-                "v:mobile": req.body.mobileNumber,
-                "v:city": req.body.city,
-                "v:message": req.body.message
+        var reciepentAddress = req.body.recieveCopy ? req.body.email : 'DEFAULT_RECIEPIENTS_EMAIL';
+               
+          const data = {
+                    from: req.body.email,
+                    to: reciepentAddress,
+                    subject: req.body.subject,
+                    message: req.body.message
             };
 
             mg.messages().send(data, (error, body) => {
--- src/app/services/contactus.service.ts
+++ src/app/services/contactus.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-const URL='https://us-central1-email-angular-cloud-function.cloudfunctions.net/contactUs';
+const URL='CLOUD_FUNCTION_URL';
 @Injectable({
   providedIn: 'root'
 })
